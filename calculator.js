e6f2a11cb533c38b53876f3f417dc95a299397ef
arinata <arinatailmiawan@gmail.com>
2021-03-25 16:15:52 +0700

    Update with backspace feature. Input numbers stored in string instead of number

--- calculator.js
+++ calculator.js
@@ -1,4 +1,8 @@
-var input = {numb1: 0, numb2: 0, flag1:0, flag2:0, operand: null};
+var input = {numb1: 0, numb2: 0, flag1:0, flag2:0, dotted:0, 
+    decCount:0, decLim:0, decMult:0.1, lastInput:0, operand: null};
+
+var numb1 = {value: '0', sign: 1, maxLength: 12, dotted: 0};
+var numb2 = {value: '0', sign: 1, maxLength: 12, dotted: 0};
 
 function add (a,b) {
 	return a+b;
@@ -13,7 +17,7 @@ function multiply (a,b) {
 }
 
 function divide (a,b) {
-	return a/b;
+    return a/b;
 }
 
 function operate (a,b,c) {
@@ -39,65 +43,132 @@ const buttonMul = document.querySelector("#buttonMul");
 const buttonEq = document.querySelector("#buttonEq");
 const buttonAdd = document.querySelector("#buttonAdd");
 const buttonSubs = document.querySelector("#buttonSubs");
+const buttonDot = document.querySelector("#buttonDot");
+const buttonBckSpc = document.querySelector("#buttonBckSpc");
 
 document.getElementById("display0").textContent = input.numb1;
 
 function limitDigit(numb){
-    if(numb/100000000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 0}).format(numb);}
-    else if(numb/10000000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 1}).format(numb);}
-    else if(numb/1000000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 2}).format(numb);}
-    else if(numb/100000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 3}).format(numb);}
-    else if(numb/10000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 4}).format(numb);}
-    else if(numb/1000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 5}).format(numb);}
-    else if(numb/100>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 6}).format(numb);}
-    else if(numb/10>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 7}).format(numb);}
-    else{return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 8}).format(numb);}
+    if(numb/100000000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 0,useGrouping:false}).format(numb);}
+    else if(numb/10000000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 1,useGrouping:false}).format(numb);}
+    else if(numb/1000000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 2,useGrouping:false}).format(numb);}
+    else if(numb/100000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 3,useGrouping:false}).format(numb);}
+    else if(numb/10000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 4,useGrouping:false}).format(numb);}
+    else if(numb/1000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 5,useGrouping:false}).format(numb);}
+    else if(numb/100>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 6,useGrouping:false}).format(numb);}
+    else if(numb/10>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 7,useGrouping:false}).format(numb);}
+    else{return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 8,useGrouping:false}).format(numb);}
+}
+
+function limitDigitDisplay(numb){
+    return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 8}).format(numb);
+}
+
+function limitDecimal(numb){
+    if(numb/100000000>1){return 10*0;}
+    else if(numb/10000000>1){return 10**(-1);}
+    else if(numb/1000000>1){return 10**(-2);}
+    else if(numb/100000>1){return 10**(-3);}
+    else if(numb/10000>1){return 10**(-4);}
+    else if(numb/1000>1){return 10**(-5);}
+    else if(numb/100>1){return 10**(-6);}
+    else if(numb/10>1){return 10**(-7);}
+    else{return 10**(-8);}
+}
+
+function numbRst(numb){
+    numb.value = '0';
+    numb.dotted = 0;
+    numb.sign = 1;    
+    numb.maxLength = 12;
 }
 
 function inputNumber(numb, input){
     if(input.flag2==1){
         input.flag2 = 0;
-        input.numb1 = 0;
+        numbRst(numb1);
     }
     if(input.flag1==0){
-        console.log('numb1');
-        if(input.numb1<100000000){input.numb1 = input.numb1*10 + numb;}
-        document.getElementById("display0").textContent = input.numb1;        
+        if(numb1.value.length<numb1.maxLength){numb1.value = numb1.value + numb;}
+        document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);        
         document.getElementById("display1").textContent = ''; 
         document.getElementById("display2").textContent = ''; 
         document.getElementById("display3").textContent = ''; 
     }
     else{
-        console.log('numb2');
-        if(input.numb2<100000000){input.numb2 = input.numb2*10 + numb;}
-        document.getElementById("display0").textContent = input.numb2;
-    }
+        if(numb2.value.length<numb2.maxLength){numb2.value = numb2.value + numb;}
+        document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb2.value)*numb2.sign); 
+    }    
 }
 
 function operand(input,operator){
-    input.flag2 = 0;
+    input.flag2=0;
     if(input.operand==null){
         input.operand = operator;
         input.flag1 = 1;
-        document.getElementById("display0").textContent = input.numb2;
+        document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb2.value));
         document.getElementById("display1").textContent = input.operand;
-        document.getElementById("display2").textContent = limitDigit(input.numb1);
+        document.getElementById("display2").textContent = limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);
         document.getElementById("display3").textContent = "";
     }
     else{
-        input.numb1 = operate(input.numb1,input.numb2,input.operand);
-        console.log(typeof input.numb1);
-        input.numb2 = 0;
-        input.operand = operator;
-        input.flag1 = 1;
-        document.getElementById("display0").textContent = input.numb2;
-        document.getElementById("display1").textContent = input.operand;
-        document.getElementById("display2").textContent = '=' + limitDigit(input.numb1);
-        document.getElementById("display3").textContent = "";
+        if(numb2.value.length==1 && numb1.value!="Infinity"){
+            input.operand = operator;
+            input.flag1 = 1;
+            document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb2.value));
+            document.getElementById("display1").textContent = input.operand;
+            document.getElementById("display2").textContent = limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);
+            document.getElementById("display3").textContent = "";
+        }else{
+            input.numb1 = parseFloat(numb1.value)*numb1.sign;
+            input.numb2 = parseFloat(numb2.value)*numb2.sign;
+            input.numb1 = operate(input.numb1,input.numb2,input.operand);
+            if(input.numb1==Infinity || isNaN(input.numb1)){
+                numb1.value = input.numb1;
+                numb1.sign = 1;
+            }
+            else if(input.numb1<0){
+                numb1.value = "0" + limitDigit((-1)*input.numb1);
+                numb1.sign = -1;
+            }else{
+                console.log(limitDigit(input.numb1));
+                numb1.value = "0" + limitDigit(input.numb1);
+                console.log(numb1.value);
+                numb1.sign = 1;
+            }
+            numb2.value = "0";
+            numb2.sign = 1;
+            numb2.dotted = 0;
+            input.operand = operator;
+            input.flag1 = 1;
+            document.getElementById("display0").textContent = numb2.value;
+            document.getElementById("display1").textContent = input.operand;
+            if(numb1.value==Infinity || isNaN(numb1.value))
+                {document.getElementById("display2").textContent = '='+numb1.value;}
+            else{document.getElementById("display2").textContent = '=' + limitDigitDisplay(limitDigit(parseFloat(numb1.value)*numb1.sign));}
+            document.getElementById("display3").textContent = "";
+        }
+        
     }
 }
 
-button0.addEventListener('click', function(e){inputNumber(0,input);});
+button0.addEventListener('click', function(e){
+    if(input.flag2==1){
+        input.flag2 = 0;
+        numbRst(numb1);
+    }
+    if(input.flag1==0){
+        if(numb1.value.length<numb1.maxLength && numb1.value.length!=1){numb1.value = numb1.value + 0;}
+        document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);        
+        document.getElementById("display1").textContent = ''; 
+        document.getElementById("display2").textContent = ''; 
+        document.getElementById("display3").textContent = ''; 
+    }
+    else{
+        if(numb2.value.length<numb2.maxLength && numb1.value.length!=1){numb2.value = numb2.value + 0;}
+        document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb2.value)*numb2.sign); 
+    }    
+});
 button1.addEventListener('click', function(e){inputNumber(1,input);});
 button2.addEventListener('click', function(e){inputNumber(2,input);});
 button3.addEventListener('click', function(e){inputNumber(3,input);});
@@ -107,6 +178,32 @@ button6.addEventListener('click', function(e){inputNumber(6,input);});
 button7.addEventListener('click', function(e){inputNumber(7,input);});
 button8.addEventListener('click', function(e){inputNumber(8,input);});
 button9.addEventListener('click', function(e){inputNumber(9,input);});
+buttonDot.addEventListener('click', function(e){
+    if(input.flag2==1){
+        input.flag2 = 0;
+        numbRst(numb1);
+    }
+    console.log(input.flag1);
+    if(input.flag1==0){
+        if(numb1.dotted==0){
+            if(numb1.value.length<numb1.maxLength){numb1.value = numb1.value + '.';}
+            document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb1.value)*numb1.sign) + '.';
+            document.getElementById("display1").textContent = ''; 
+            document.getElementById("display2").textContent = ''; 
+            document.getElementById("display3").textContent = '';        
+            numb1.dotted = 1;
+            numb1.maxLength ++;
+        }
+    }
+    else{
+        if(numb2.dotted==0){
+            if(numb2.value.length<numb2.maxLength){numb2.value = numb2.value + '.';}
+            document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb2.value)*numb2.sign) + '.'; 
+            numb2.dotted = 1;
+            numb2.maxLength ++;
+        }
+    }
+});
 
 buttonClr.addEventListener('click',function(e){
     input.numb1=0;
@@ -114,6 +211,8 @@ buttonClr.addEventListener('click',function(e){
     input.flag1=0;
     input.flag2=0;
     input.operand=null;
+    numbRst(numb1);
+    numbRst(numb2);
     document.getElementById("display0").textContent = 0;
     document.getElementById("display1").textContent = "";
     document.getElementById("display2").textContent = "";
@@ -137,22 +236,74 @@ buttonDiv.addEventListener('click',function(e){
 });
 
 buttonEq.addEventListener('click',function(e){
+    input.dotted=0;
+    input.decMult=0.1;
     if(input.operand==null){
-        if(input.numb1!=0){
-            document.getElementById("display0").textContent = limitDigit(input.numb1);
-            document.getElementById("display1").textContent = '=' + limitDigit(input.numb1);        
+        if(numb1.value!='0'){
+            document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);
+            document.getElementById("display1").textContent = '=' + limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);        
             input.flag2 = 1;
         }
     }
     else{
-        document.getElementById("display1").textContent = input.numb2;
+        input.numb1 = parseFloat(numb1.value)*numb1.sign;
+        input.numb2 = parseFloat(numb2.value)*numb2.sign;
+        document.getElementById("display1").textContent = limitDigitDisplay(input.numb2);
         document.getElementById("display2").textContent = input.operand;
-        document.getElementById("display3").textContent = limitDigit(input.numb1);
+        document.getElementById("display3").textContent = limitDigitDisplay(input.numb1);
         input.numb1 = operate(input.numb1,input.numb2,input.operand);
+        if(input.numb1==Infinity || isNaN(input.numb1)){
+            numb1.value = input.numb1;
+            numb1.sign = 1;
+        }
+        else if(input.numb1<0){
+            numb1.value = "0" + limitDigit((-1)*input.numb1);
+            numb1.sign = -1;
+        }else{
+            numb1.value = "0" + limitDigit(input.numb1);
+            numb1.sign = 1;
+        }
+        numbRst(numb2);
         input.numb2 = 0;
         input.flag2 = 1;
         input.flag1 = 0;
-        document.getElementById("display0").textContent = '=' + limitDigit(input.numb1);
+        if(numb1.value==Infinity || isNaN(numb1.value))
+            {
+                console.log(numb1.value);
+                console.log('yangini?');
+                document.getElementById("display0").textContent = '='+numb1.value;}
+        else{
+            console.log('masuk?');
+            document.getElementById("display0").textContent = '=' + limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);}
         input.operand = null;
     }
+});
+
+
+
+buttonBckSpc.addEventListener('click', function(e){
+    if(input.flag2==0){
+        if(input.flag1==0){
+            if(numb1.value.length>1){
+                numb1.value = numb1.value.substring(0,numb1.value.length-1);
+                if(numb1.value.length==1){numb1.sign=1;}
+                document.getElementById("display0").textContent = parseFloat(numb1.value)*numb1.sign; 
+            }
+        }
+        if(input.flag1==1){
+            if(numb2.value.length>1){
+                numb2.value = numb2.value.substring(0,numb2.value.length-1);
+                if(numb2.value.length==1){numb2.sign=1;}
+                document.getElementById("display0").textContent = parseFloat(numb2.value)*numb2.sign;
+            }
+            else if(numb2.value.length==1){
+                document.getElementById("display2").textContent = '';
+                document.getElementById("display1").textContent = '';
+                document.getElementById("display0").textContent = parseFloat(numb1.value)*numb1.sign; 
+                input.operand=null;
+                input.flag1=0;
+                numbRst(numb2);
+            }
+        }
+    }    
 });
