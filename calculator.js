9423b8fb6f365f20b891c2b004b2bc06746e6df7
arinata <arinatailmiawan@gmail.com>
2021-03-26 08:46:52 +0700

    Add keyboard support

--- calculator.js
+++ calculator.js
@@ -1,32 +1,9 @@
-var input = {numb1: 0, numb2: 0, flag1:0, flag2:0, dotted:0, 
-    decCount:0, decLim:0, decMult:0.1, lastInput:0, operand: null};
+/*operational variable definition*/
+var input = {numb1: 0, numb2: 0, flag1:0, flag2:0, operand: null};  //store operation and state
+var numb1 = {value: '0', sign: 1, maxLength: 12, dotted: 0};        //store first number and operation result
+var numb2 = {value: '0', sign: 1, maxLength: 12, dotted: 0};        //store second number
 
-var numb1 = {value: '0', sign: 1, maxLength: 12, dotted: 0};
-var numb2 = {value: '0', sign: 1, maxLength: 12, dotted: 0};
-
-function add (a,b) {
-	return a+b;
-}
-
-function subtract (a,b) {
-	return a-b;
-}
-
-function multiply (a,b) {
-    return a*b;
-}
-
-function divide (a,b) {
-    return a/b;
-}
-
-function operate (a,b,c) {
-    if(c=='+'){return(add(a,b));}
-    else if(c=='-'){return(subtract(a,b));}
-    else if(c=='×'){return(multiply(a,b));}
-    else if(c=='÷'){return(divide(a,b));}
-}
-
+/*object variable definition*/
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -46,7 +23,21 @@ const buttonSubs = document.querySelector("#buttonSubs");
 const buttonDot = document.querySelector("#buttonDot");
 const buttonBckSpc = document.querySelector("#buttonBckSpc");
 
+/*initialization*/
 document.getElementById("display0").textContent = input.numb1;
+
+/*function declaration*/
+function add (a,b) {return a+b;}
+function subtract (a,b) {return a-b;}
+function multiply (a,b) {return a*b;}
+function divide (a,b) {return a/b;}
+
+function operate (a,b,c) {
+    if(c=='+'){return(add(a,b));}
+    else if(c=='-'){return(subtract(a,b));}
+    else if(c=='×'){return(multiply(a,b));}
+    else if(c=='÷'){return(divide(a,b));}
+}
 
 function limitDigit(numb){
     if(numb/100000000>1){return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 0,useGrouping:false}).format(numb);}
@@ -60,20 +51,9 @@ function limitDigit(numb){
     else{return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 8,useGrouping:false}).format(numb);}
 }
 
+//limitDigitDisplay used to give comma separator for thousands, millions, billions ...
 function limitDigitDisplay(numb){
     return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,maximumFractionDigits: 8}).format(numb);
-}
-
-function limitDecimal(numb){
-    if(numb/100000000>1){return 10*0;}
-    else if(numb/10000000>1){return 10**(-1);}
-    else if(numb/1000000>1){return 10**(-2);}
-    else if(numb/100000>1){return 10**(-3);}
-    else if(numb/10000>1){return 10**(-4);}
-    else if(numb/1000>1){return 10**(-5);}
-    else if(numb/100>1){return 10**(-6);}
-    else if(numb/10>1){return 10**(-7);}
-    else{return 10**(-8);}
 }
 
 function numbRst(numb){
@@ -152,33 +132,7 @@ function operand(input,operator){
     }
 }
 
-button0.addEventListener('click', function(e){
-    if(input.flag2==1){
-        input.flag2 = 0;
-        numbRst(numb1);
-    }
-    if(input.flag1==0){
-        if(numb1.value.length<numb1.maxLength && numb1.value.length!=1){numb1.value = numb1.value + 0;}
-        document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);        
-        document.getElementById("display1").textContent = ''; 
-        document.getElementById("display2").textContent = ''; 
-        document.getElementById("display3").textContent = ''; 
-    }
-    else{
-        if(numb2.value.length<numb2.maxLength && numb1.value.length!=1){numb2.value = numb2.value + 0;}
-        document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb2.value)*numb2.sign); 
-    }    
-});
-button1.addEventListener('click', function(e){inputNumber(1,input);});
-button2.addEventListener('click', function(e){inputNumber(2,input);});
-button3.addEventListener('click', function(e){inputNumber(3,input);});
-button4.addEventListener('click', function(e){inputNumber(4,input);});
-button5.addEventListener('click', function(e){inputNumber(5,input);});
-button6.addEventListener('click', function(e){inputNumber(6,input);});
-button7.addEventListener('click', function(e){inputNumber(7,input);});
-button8.addEventListener('click', function(e){inputNumber(8,input);});
-button9.addEventListener('click', function(e){inputNumber(9,input);});
-buttonDot.addEventListener('click', function(e){
+function buttonDotFunc(){
     if(input.flag2==1){
         input.flag2 = 0;
         numbRst(numb1);
@@ -203,9 +157,27 @@ buttonDot.addEventListener('click', function(e){
             numb2.maxLength ++;
         }
     }
-});
+}
 
-buttonClr.addEventListener('click',function(e){
+function buttonZero(){
+    if(input.flag2==1){
+        input.flag2 = 0;
+        numbRst(numb1);
+    }
+    if(input.flag1==0){
+        if(numb1.value.length<numb1.maxLength && numb1.value.length!=1){numb1.value = numb1.value + 0;}
+        document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);        
+        document.getElementById("display1").textContent = ''; 
+        document.getElementById("display2").textContent = ''; 
+        document.getElementById("display3").textContent = ''; 
+    }
+    else{
+        if(numb2.value.length<numb2.maxLength && numb1.value.length!=1){numb2.value = numb2.value + 0;}
+        document.getElementById("display0").textContent = limitDigitDisplay(parseFloat(numb2.value)*numb2.sign); 
+    }    
+}
+
+function buttonClrFunc(){
     input.numb1=0;
     input.numb2=0;
     input.flag1=0;
@@ -217,25 +189,9 @@ buttonClr.addEventListener('click',function(e){
     document.getElementById("display1").textContent = "";
     document.getElementById("display2").textContent = "";
     document.getElementById("display3").textContent = "";
-});
+}
 
-buttonAdd.addEventListener('click',function(e){
-    operand(input,'+')
-});
-
-buttonSubs.addEventListener('click',function(e){
-    operand(input,'-')
-});
-
-buttonMul.addEventListener('click',function(e){
-    operand(input,'×')
-});
-
-buttonDiv.addEventListener('click',function(e){
-    operand(input,'÷')
-});
-
-buttonEq.addEventListener('click',function(e){
+function buttonEqFunc(){
     input.dotted=0;
     input.decMult=0.1;
     if(input.operand==null){
@@ -270,18 +226,14 @@ buttonEq.addEventListener('click',function(e){
         if(numb1.value==Infinity || isNaN(numb1.value))
             {
                 console.log(numb1.value);
-                console.log('yangini?');
                 document.getElementById("display0").textContent = '='+numb1.value;}
         else{
-            console.log('masuk?');
             document.getElementById("display0").textContent = '=' + limitDigitDisplay(parseFloat(numb1.value)*numb1.sign);}
         input.operand = null;
     }
-});
+}
 
-
-
-buttonBckSpc.addEventListener('click', function(e){
+function buttonBckSpcFunc(){
     if(input.flag2==0){
         if(input.flag1==0){
             if(numb1.value.length>1){
@@ -306,4 +258,50 @@ buttonBckSpc.addEventListener('click', function(e){
             }
         }
     }    
+}
+
+/*event capture for button click definition*/
+button0.addEventListener('click', function(e){buttonZero()});
+button1.addEventListener('click', function(e){inputNumber(1,input);});
+button2.addEventListener('click', function(e){inputNumber(2,input);});
+button3.addEventListener('click', function(e){inputNumber(3,input);});
+button4.addEventListener('click', function(e){inputNumber(4,input);});
+button5.addEventListener('click', function(e){inputNumber(5,input);});
+button6.addEventListener('click', function(e){inputNumber(6,input);});
+button7.addEventListener('click', function(e){inputNumber(7,input);});
+button8.addEventListener('click', function(e){inputNumber(8,input);});
+button9.addEventListener('click', function(e){inputNumber(9,input);});
+buttonDot.addEventListener('click', function(e){buttonDotFunc()});
+buttonClr.addEventListener('click',function(e){buttonClrFunc()});
+buttonAdd.addEventListener('click',function(e){operand(input,'+')});
+buttonSubs.addEventListener('click',function(e){operand(input,'-')});
+buttonMul.addEventListener('click',function(e){operand(input,'×')});
+buttonDiv.addEventListener('click',function(e){operand(input,'÷')});
+buttonEq.addEventListener('click',function(e){buttonEqFunc()});
+buttonBckSpc.addEventListener('click', function(e){buttonBckSpcFunc()});
+
+/*event capture for keyboard input definition*/
+window.addEventListener('keydown', function(e){
+    switch (e.key){
+        case '1': inputNumber(1,input); break;
+        case '2': inputNumber(2,input); break;
+        case '3': inputNumber(3,input); break;
+        case '4': inputNumber(4,input); break;
+        case '5': inputNumber(5,input); break;
+        case '6': inputNumber(6,input); break;
+        case '7': inputNumber(7,input); break;
+        case '8': inputNumber(8,input); break;
+        case '9': inputNumber(9,input); break;
+        case '0': buttonZero(); break;
+        case '.': buttonDotFunc(); break;
+        case 'c': buttonClrFunc(); break;
+        case '+': operand(input,'+'); break;
+        case '-': operand(input,'-'); break;
+        case '*': operand(input,'×'); break;
+        case 'x': operand(input,'×'); break;
+        case '/': operand(input,'÷'); break;
+        case 'Enter': buttonEqFunc(); break;
+        case '=': buttonEqFunc(); break;
+        case 'Backspace': buttonBckSpcFunc(); break;
+    }
 });
